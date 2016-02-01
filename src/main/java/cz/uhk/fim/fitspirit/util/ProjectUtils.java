d7f57fb8a8afd9d62871ae281d92130539891f7e
Skoreto <TomSkorepa@gmail.com>
2016-02-01 17:47:56 +0100

    Renamed project and servlet.
    Rewritten web.xml dependencies.
    Rewritten log4j.properties.
    Changed project path for folder 'uploads'.

--- src/main/java/cz/uhk/fim/fitspirit/util/ProjectUtils.java
+++ src/main/java/cz/uhk/fim/fitspirit/util/ProjectUtils.java
@@ -24,8 +24,8 @@ public final class ProjectUtils {
 	/**
 	 * Nutné pøenastavit cestu ke složce "uploads" v projektu.
 	 */
-	private static String myProjectPath = "C:\\Users\\Tomas\\Documents\\workspace-sts-3.7.2.RELEASE\\petclinic\\src\\main\\webapp\\static\\uploads";
-
+	private static String myProjectPath = "C:\\Users\\Tomas\\Documents\\workspace-sts-3.7.2.RELEASE\\fitspirit\\src\\main\\webapp\\static\\uploads";
+	
 	public static String getMyProjectPath() {
 		return myProjectPath;
 	}	
